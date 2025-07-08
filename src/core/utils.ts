3e1cc64885fd6f16e48bdb24d8c36d4cafac1d39
Yam Borodetsky <yam@yam.codes>
2025-07-09 01:57:55 +0500

    Enhance error handling in createApp function: add formatZodErrors utility to format Zod validation errors and integrate it into the default hook of OpenAPIHono for improved error reporting and maintainability.

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,20 +1,43 @@
-import { OpenAPIHono } from "@hono/zod-openapi";
-import type { Env, Schema } from "hono";
+import { OpenAPIHono, type OpenAPIHonoOptions } from "@hono/zod-openapi";
+import type { Env, Hono, Schema, ValidationTargets } from "hono";
+import { HTTPException } from "hono/http-exception";
 import type { EmptyObject } from "type-fest";
+import type { ZodError } from "zod";
+import { StatusCodes } from "@/shared/constants";
 import {
 	auth,
 	type JwtPayload,
 	type TokenVariables,
 } from "./middleware/auth.middleware";
 
+export function formatZodErrors(
+	result: { target: keyof ValidationTargets } & {
+		success: false;
+		error: ZodError;
+	},
+): { path: string; message: string }[] {
+	return result.error.errors.map((error) => ({
+		path: error.path.join("."),
+		message: error.message,
+	}));
+}
+
+type HonoInit<E extends Env> = ConstructorParameters<typeof Hono>[0] &
+	OpenAPIHonoOptions<E>;
+
 /**
  * Options for the createApp function.
  */
-export type CreateAppOptions = {
+export type CreateAppOptions<E extends Env = Env> = {
 	/**
 	 * Whether to use authentication middleware.
 	 */
 	auth?: boolean;
+
+	/**
+	 * The initial options for the Hono app. (This is what you pass in `new Hono(...)` or `new OpenAPIHono(...)`)
+	 */
+	init?: HonoInit<E>;
 };
 
 /**
@@ -33,8 +56,19 @@ export function createApp<
 	E extends Env = Env,
 	S extends Schema = EmptyObject,
 	BasePath extends string = "/",
->(options?: CreateAppOptions) {
-	const app = new OpenAPIHono<E, S, BasePath>();
+>(options?: CreateAppOptions<E>) {
+	const app = new OpenAPIHono<E, S, BasePath>({
+		defaultHook: (result, { json }) => {
+			if (result.success) return;
+			throw new HTTPException(StatusCodes.UNPROCESSABLE_ENTITY, {
+				cause: result.error,
+				res: json({
+					errors: formatZodErrors(result),
+				}),
+			});
+		},
+		...options?.init,
+	});
 	if (options?.auth) {
 		app.use("*", auth());
 		app.openAPIRegistry.registerComponent("securitySchemes", "Token", {
